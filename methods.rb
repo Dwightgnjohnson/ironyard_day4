403d8b69f9e5d48cd78a0005759892389ea448c1
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-09-25 11:25:51 -0500

    add scoop and method

---
+++ methods.rb
@@ -0,0 +1,51 @@
+### METHODS ###
+# All methods in ruby returns a value
+
+def greetings(name) #only defined a method
+  puts "Good News Everyone!"
+  puts "#{name} is drunk."
+end
+
+def crazy(name)
+  "Kyle is #{name}"
+end
+
+greetings(crazy("Kevin"))
+
+
+# def astrid
+#   23
+# end
+#
+# def dwight
+#   17
+# end
+#
+# def kirt
+#   6 + astrid
+# end
+#
+# p kirt + dwight
+
+# def greetings(name) #only defined a method
+#   puts "Good News Everyone!"
+#   puts "#{name} is drunk."
+# end
+#
+# def crazy(something)
+#   "Kyle is #{something}"
+# end
+
+#puts is a method and it return nil
+
+#p greetings("Kevin")
+
+
+# def example
+#   5 # won't even look at 5 bc ruby reads line by line
+#   return 4 #return is not neccessary BUT If there is a return it takes over the default
+# end
+# p example + 3
+
+#puts crazy("cool")
+#greetings "Kevin" #or greetings("Kevin") the parenthesis are optional
